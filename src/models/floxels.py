3aa5f942f617e02e18f64cdedf86c5fa0e2c5e88
Kin <kinzhangglimmer@gmail.com>
2025-11-26 13:06:57 +0100

    feat(multi-frame): floxels multi-frame re-organize code. More things added into this commit ref to Floxels' paper: * add lr_scheduler * truncate_dist in DT as floxel paper states. * gamma flow regularizer. while the 5 frames makes error higher, so Hanqiu and I are checking the re-implementation.

--- src/models/floxels.py
+++ src/models/floxels.py
@@ -15,14 +15,10 @@
 
 import dztimer, torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 from sklearn.cluster import DBSCAN
-
-import FastGeodis # extra package
-
-from .basic.nsfp_module import VoxelGrid, EarlyStopping
-from .basic import cal_pose0to1
-
+from .basic.opt_module import VoxelGrid, EarlyStopping, DT
+from .basic import wrap_batch_pcs
 
 def cluster(pc, min_samples=4, eps=0.5):
     pc_np = pc.detach().to("cpu").numpy()
@@ -49,66 +45,12 @@ def cluster_loss(flow, clusters):
     l2_loss_per_point = (flow - mean_expanded).norm(dim=1)
     return l2_loss_per_point
 
-
-class DT:
-    def __init__(self, pts, pmin, pmax, grid_factor, device='cuda:0'):
-        self.device = device
-        self.grid_factor = grid_factor
-        
-        sample_x = ((pmax[0] - pmin[0]) * grid_factor).ceil().int() + 2
-        sample_y = ((pmax[1] - pmin[1]) * grid_factor).ceil().int() + 2
-        sample_z = ((pmax[2] - pmin[2]) * grid_factor).ceil().int() + 2
-        
-        self.Vx = torch.linspace(0, sample_x, sample_x+1, device=self.device)[:-1] / grid_factor + pmin[0]
-        self.Vy = torch.linspace(0, sample_y, sample_y+1, device=self.device)[:-1] / grid_factor + pmin[1]
-        self.Vz = torch.linspace(0, sample_z, sample_z+1, device=self.device)[:-1] / grid_factor + pmin[2]
-        
-        # NOTE: build a binary image first, with 0-value occuppied points
-        grid_x, grid_y, grid_z = torch.meshgrid(self.Vx, self.Vy, self.Vz, indexing="ij")
-        self.grid = torch.stack([grid_x.unsqueeze(-1), grid_y.unsqueeze(-1), grid_z.unsqueeze(-1)], -1).float().squeeze()
-        H, W, D, _ = self.grid.size()
-        pts_mask = torch.ones(H, W, D, device=device)
-        self.pts_sample_idx_x = ((pts[:,0:1] - self.Vx[0]) * self.grid_factor).round()
-        self.pts_sample_idx_y = ((pts[:,1:2] - self.Vy[0]) * self.grid_factor).round()
-        self.pts_sample_idx_z = ((pts[:,2:3] - self.Vz[0]) * self.grid_factor).round()
-        pts_mask[self.pts_sample_idx_x.long(), self.pts_sample_idx_y.long(), self.pts_sample_idx_z.long()] = 0.
-        
-        iterations = 1
-        image_pts = torch.zeros(H, W, D, device=device).unsqueeze(0).unsqueeze(0)
-        pts_mask = pts_mask.unsqueeze(0).unsqueeze(0)
-        self.D = FastGeodis.generalised_geodesic3d(
-            image_pts, pts_mask, [1./self.grid_factor, 1./self.grid_factor, 1./self.grid_factor], 1e10, 0.0, iterations
-        ).squeeze()
-            
-    def torch_bilinear_distance(self, Y):
-        H, W, D = self.D.size()
-        target = self.D[None, None, ...]
-        
-        sample_x = ((Y[:,0:1] - self.Vx[0]) * self.grid_factor).clip(0, H-1)
-        sample_y = ((Y[:,1:2] - self.Vy[0]) * self.grid_factor).clip(0, W-1)
-        sample_z = ((Y[:,2:3] - self.Vz[0]) * self.grid_factor).clip(0, D-1)
-        
-        sample = torch.cat([sample_x, sample_y, sample_z], -1)
-        
-        # NOTE: normalize samples to [-1, 1]
-        sample = 2 * sample
-        sample[...,0] = sample[...,0] / (H-1)
-        sample[...,1] = sample[...,1] / (W-1)
-        sample[...,2] = sample[...,2] / (D-1)
-        sample = sample -1
-        
-        sample_ = torch.cat([sample[...,2:3], sample[...,1:2], sample[...,0:1]], -1)
-        
-        # NOTE: reshape to match 5D volumetric input
-        dist = F.grid_sample(target, sample_.view(1,-1,1,1,3), mode="bilinear", align_corners=True).view(-1)
-        return dist
-
 class Floxels(nn.Module):
     # default parameters from Sec 3.1
     def __init__(self, grid_factor=10., itr_num=500, lr=0.05, min_delta=0.01, early_patience=250,
-                 verbose=False, point_cloud_range = [-51.2, -51.2, -3, 51.2, 51.2, 3]):
+                 verbose=False, point_cloud_range = [-51.2, -51.2, -3, 51.2, 51.2, 3], voxel_size=0.5, num_frames=3,
+                 cluster_weight=0.6):
         super().__init__()
-        
 
         self.grid_factor = grid_factor # grid cell size=1/grid_factor.
         self.iteration_num = itr_num
@@ -117,91 +59,102 @@ class Floxels(nn.Module):
         self.early_patience = early_patience
         self.verbose = verbose
         self.point_cloud_range = point_cloud_range
+        self.voxel_size = voxel_size
+        self.num_frames = num_frames
+
+        self.cluster_weight = cluster_weight
+
         self.timer = dztimer.Timing()
         self.timer.start("Floxels Model Inference")
-        print(f"\n---LOG [model]: Floxels setup itr_num: {itr_num}, lr: {lr}, early_patience: {early_patience}.")
+        print(f"\n---LOG [model]: Floxels setup num_frame: {num_frames}, itr_num: {itr_num}, lr: {lr}, early_patience: {early_patience} with min_delta: {min_delta}.")
             
     def optimize(self, dict2loss):
         device = dict2loss['pc0'].device
 
-        # NOTE(Qingwen): don't know why, but it must be initialized every optimization time.
-        self.timer[5].start("Network Initialization")
-        net = VoxelGrid()
+        self.timer[1].start("Network Initialization")
+        net = VoxelGrid(point_cloud_range=self.point_cloud_range, voxel_size=self.voxel_size)
         net = net.to(device)
-
         net.train()
-        self.timer[5].stop()
+        self.timer[1].stop()
 
+        self.timer[2].start("DT Map Building")
         pc0 = dict2loss['pc0']
-        pc1 = dict2loss['pc1']
-        if 'pch1' in dict2loss:
-            pch = dict2loss['pch1']
-            
-        clusters = cluster(pc0)
-
         pc0_min = torch.min(pc0.squeeze(0), 0)[0]
-        pc1_min = torch.min(pc1.squeeze(0), 0)[0]
-        pc0_max = torch.max(pc0.squeeze(0), 0)[0]
-        pc1_max = torch.max(pc1.squeeze(0), 0)[0]
-        
-        xmin_int, ymin_int, zmin_int = torch.floor(torch.where(pc0_min<pc1_min, pc0_min, pc1_min) * self.grid_factor-1) / self.grid_factor
-        xmax_int, ymax_int, zmax_int = torch.ceil(torch.where(pc0_max>pc1_max, pc0_max, pc1_max)* self.grid_factor+1) / self.grid_factor
-        # print('xmin: {}, xmax: {}, ymin: {}, ymax: {}, zmin: {}, zmax: {}'.format(xmin_int, xmax_int, ymin_int, ymax_int, zmin_int, zmax_int))
-        
-        # NOTE: build DT map
-        dt = DT(pc1.clone().squeeze(0).to(device), (xmin_int, ymin_int, zmin_int), (xmax_int, ymax_int, zmax_int), self.grid_factor, device)
+        pc0_max = torch.max(pc0.squeeze(0), 0)[0]        
+        dict2loss.pop('pc0', None)
 
-        if 'pch1' in dict2loss:
-            pch_min = torch.min(pch.squeeze(0), 0)[0]
-            pch_max = torch.max(pch.squeeze(0), 0)[0]
-            xmin_int_h, ymin_int_h, zmin_int_h = torch.floor(torch.min(pc1_min, pch_min) * self.grid_factor-1) / self.grid_factor
-            xmax_int_h, ymax_int_h, zmax_int_h = torch.ceil(torch.max(pc1_max, pch_max)* self.grid_factor+1) / self.grid_factor
-            dt_h = DT(pch.clone().squeeze(0).to(device), (xmin_int_h, ymin_int_h, zmin_int_h), (xmax_int_h, ymax_int_h, zmax_int_h), self.grid_factor, device)
+        dt_dict = {}
+        for key_ in dict2loss.keys():
+            pc_aim = dict2loss[key_]
+            pc2_min = torch.min(pc_aim.squeeze(0), 0)[0]
+            pc2_max = torch.max(pc_aim.squeeze(0), 0)[0]
+            
+            xmin_int, ymin_int, zmin_int = torch.floor(torch.where(pc0_min<pc2_min, pc0_min, pc2_min) * self.grid_factor-1) / self.grid_factor
+            xmax_int, ymax_int, zmax_int = torch.ceil(torch.where(pc0_max>pc2_max, pc0_max, pc2_max)* self.grid_factor+1) / self.grid_factor
+            # print('xmin: {}, xmax: {}, ymin: {}, ymax: {}, zmin: {}, zmax: {}'.format(xmin_int, xmax_int, ymin_int, ymax_int, zmin_int, zmax_int))
+            
+            # NOTE: build DT map
+            dt_dict[key_] = DT(pc_aim.clone().squeeze(0).to(device), (xmin_int, ymin_int, zmin_int), (xmax_int, ymax_int, zmax_int), self.grid_factor, device)
+        self.timer[2].stop()
 
+        self.timer[3].start("Clustering")
+        clusters = cluster(pc0)
+        self.timer[3].stop()
         params = net.parameters()
+
         best_forward = {'loss': torch.inf}
 
         optimizer = torch.optim.Adam(params, lr=self.lr, weight_decay=0)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20], gamma=0.5)
         early_stopping = EarlyStopping(patience=self.early_patience, min_delta=self.min_delta)
 
-        for itr_ in range(self.iteration_num):
+        frame_keys = sorted([key for key in dict2loss.keys() if key.startswith('pch')], reverse=False)
+        self.timer[4].start("Optimization")
+        for _ in range(self.iteration_num):
             optimizer.zero_grad()
-            self.timer[1].start("Network Time")
-
-            self.timer[1][0].start("Forward")
+            self.timer[4][1].start("Network Time")
             forward_flow = net(pc0)
-            self.timer[1][0].stop()
-            pc0_to_pc1 = pc0 + forward_flow
-            self.timer[1].stop()
+            self.timer[4][1].stop()
 
-            self.timer[2].start("loss")
-            loss = dt.torch_bilinear_distance(pc0_to_pc1.squeeze(0)).mean()
-            if 'pch1' in dict2loss:
-                pch_to_pc0 = pc0 - forward_flow
-                loss *= 0.5
-                loss += dt_h.torch_bilinear_distance(pch_to_pc0.squeeze(0)).mean() * 0.5
+            self.timer[4][2].start("loss")
+            loss = dt_dict['pc1'].torch_bilinear_distance((pc0 + forward_flow).squeeze(0), truncate_dist=5.0).mean()
+
+            # (0, 'pch1s'), (1, 'pch2s'), ...
+            for time_index, frame_key in enumerate(reversed(frame_keys)):
+                dt = dt_dict[frame_key]
+                pesudo_pc = pc0 - (time_index + 1) * forward_flow
+                # since time_index starts from 0
+                loss += dt.torch_bilinear_distance(pesudo_pc.squeeze(0), truncate_dist=5.0).mean() * pow(1/(time_index+1), 2)
+            self.timer[4][2].stop()
+
+            # FIXME: lambda_d weight didn't specify before. 
+            # loss *= lambda_d # ? ask Hanqiu later
+            
             cl_loss = cluster_loss(
                 forward_flow[clusters >= 0],
                 clusters[clusters >= 0]
             )
-            loss += cl_loss.mean() * 0.6
+            loss += cl_loss.mean() * self.cluster_weight
 
-            self.timer[2].stop()
+            # FIXME paper didn't say lambda_gamma's value.....
+            loss += (0.000005 / (time_index + 1)) * torch.norm(forward_flow, p=2)
 
             if loss <= best_forward['loss']:
                 best_forward['loss'] = loss.item()
-                best_forward['flow'] = pc0_to_pc1 - pc0
+                best_forward['flow'] = forward_flow
 
             if early_stopping.step(loss) and 'flow' in best_forward: # at least one step
                 break
 
-            self.timer[3].start("Loss Backward")
+            self.timer[4][3].start("Loss Backward")
             loss.backward()
-            self.timer[3].stop()
+            self.timer[4][3].stop()
 
-            self.timer[4].start("Optimizer Step")
+            self.timer[4][4].start("Optimizer Step")
             optimizer.step()
-            self.timer[4].stop()
+            scheduler.step()
+            self.timer[4][4].stop()
+        self.timer[4].stop()
 
         if self.verbose:
             self.timer.print(random_colors=True, bold=True)
@@ -219,48 +172,31 @@ class Floxels(nn.Module):
     
     def forward(self, batch):
         batch_sizes = len(batch["pose0"])
+        self.timer[0].start("Data Preprocess")
+        pcs_dict = wrap_batch_pcs(batch, num_frames=self.num_frames)
+        self.timer[0].stop()
 
-        pose_flows = []
         batch_final_flow = []
         for batch_id in range(batch_sizes):
-            self.timer[0].start("Data Processing")
-            pc0 = batch["pc0"][batch_id]
-            pc1 = batch["pc1"][batch_id]
-            if "pch1" in batch:
-                pch = batch["pch1"][batch_id]
-                selected_pch, rmh = self.range_limit_(pch)
-                pose_hto1 = cal_pose0to1(batch["poseh1"][batch_id], batch["pose1"][batch_id])
-                transform_pch = selected_pch @ pose_hto1[:3, :3].T + pose_hto1[:3, 3]
-            
+            pc0 = pcs_dict["pc0s"][batch_id,...]
+            pc1 = pcs_dict["pc1s"][batch_id,...]
+            selected_pc0, rm0 = self.range_limit_(pc0)
+            selected_pc1, rm1 = self.range_limit_(pc1)
+            pchs = {}
+            for i in range(1, self.num_frames - 1):
+                pchs[f'pch{i}'] = self.range_limit_(pcs_dict[f'pch{i}s'][batch_id,...])[0]
 
             selected_pc0, rm0 = self.range_limit_(pc0)
             selected_pc1, rm1 = self.range_limit_(pc1)
-            
-            self.timer[0][0].start("pose")
-            if 'ego_motion' in batch:
-                pose_0to1 = batch['ego_motion'][batch_id]
-            else:
-                pose_0to1 = cal_pose0to1(batch["pose0"][batch_id], batch["pose1"][batch_id])
-            self.timer[0][0].stop()
-            
-            self.timer[0][1].start("transform")
-            # transform selected_pc0 to pc1
-            transform_pc0 = selected_pc0 @ pose_0to1[:3, :3].T + pose_0to1[:3, 3]
-            self.timer[0][1].stop()
-            pose_flows.append(transform_pc0 - selected_pc0)
-
-            self.timer[0].stop()
 
             # since pl in val and test mode will disable_grad.
             with torch.inference_mode(False):
                 with torch.enable_grad():
                     dict2loss = {
-                        'pc0': transform_pc0.clone().detach(), #.requires_grad_(True),
+                        'pc0': selected_pc0.clone().detach(), #.requires_grad_(True),
                         'pc1': selected_pc1.clone().detach(), #.requires_grad_(True)
                     }
-                    if "pch1" in batch:
-                        dict2loss["pch1"] = transform_pch.clone().detach()
-
+                    dict2loss.update(pchs)
                     model_res = self.optimize(dict2loss)
             
             final_flow = torch.zeros_like(pc0)
@@ -268,7 +204,8 @@ class Floxels(nn.Module):
             batch_final_flow.append(final_flow)
 
         res_dict = {"flow": batch_final_flow,
-                    "pose_flow": pose_flows
+                    "loss": [model_res["loss"]],
+                    "pose_flow": pcs_dict["pose_flows"]  # using identity here
                     }
         
         return res_dict
